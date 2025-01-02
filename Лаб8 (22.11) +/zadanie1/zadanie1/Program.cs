6df8a4e0e486aa390874d425b026e77a0c8b6447
Tourecs <sverlov2006@mail.ru>
2025-01-03 03:46:29 +0600

    Add files via upload

---
+++ Лаб8 (22.11) +/zadanie1/zadanie1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace aip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество подмассивов:");
+            int arraynum = Convert.ToInt32(Console.ReadLine());
+            int[][] jaggedarray = new int[arraynum][];
+
+            for (int i = 0; i < arraynum; i++)
+            {
+                Console.WriteLine($"Введите количество элементов для подмассива номер {i + 1}:");
+                int elnum = Convert.ToInt32(Console.ReadLine());
+                jaggedarray[i] = new int[elnum];
+
+                Console.WriteLine($"Введите {elnum} элементов для подмассива номер {i + 1} через пробел:");
+                string[] input = Console.ReadLine().Split(' ');
+
+                for (int k = 0; k < elnum; k++)
+                {
+                    jaggedarray[i][k] = Convert.ToInt32(input[k]);
+                }
+            }
+
+            for (int i = 0; i < jaggedarray.Length; i++)
+            {
+                int maxel = int.MinValue;
+                int sum = 0;
+
+                foreach (var number in jaggedarray[i])
+                {
+                    sum += number;
+                    if (number > maxel)
+                    {
+                        maxel = number;
+                    }
+                }
+                if (maxel > (sum - maxel))
+                {
+                    Console.WriteLine($"В массиве под номером {i + 1} максимальный элемент больше суммы остальных");
+                }
+            }
+        }
+    }
+}
---
+++ Лаб8 (22.11) +/zadanie2/zadanie2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace aip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество подмассивов:");
+            int arraynum = Convert.ToInt32(Console.ReadLine());
+            int[][] jaggedarray = new int[arraynum][];
+
+            for (int i = 0; i < arraynum; i++)
+            {
+                Console.WriteLine($"Введите количество элементов для подмассива номер {i + 1}:");
+                int elnum = Convert.ToInt32(Console.ReadLine());
+                jaggedarray[i] = new int[elnum];
+
+                Console.WriteLine($"Введите {elnum} элементов для подмассива номер {i + 1} через пробел:");
+                string[] input = Console.ReadLine().Split(' ');
+
+                for (int k = 0; k < elnum; k++)
+                {
+                    jaggedarray[i][k] = Convert.ToInt32(input[k]);
+                }
+            }
+
+            Nullable<int> minLength = null;
+
+            for (int i = 0; i < jaggedarray.Length; i++)
+            {
+                if (jaggedarray[i].Length < 2)
+                    continue;
+
+
+                int diff = jaggedarray[i][0] - jaggedarray[i][1];
+                bool ybivaet = true;
+
+                for (int k = 0; k < jaggedarray[i].Length - 1; k++)
+                {
+                    if (jaggedarray[i][k] - jaggedarray[i][k + 1] != diff)
+                    {
+                        ybivaet = false;
+                        break;
+                    }
+                }
+
+
+                if (ybivaet)
+                {
+                    int length = jaggedarray[i].Length;
+                    Console.WriteLine($"Длина убывающего подмассива номер {i + 1} равна {length}");
+
+
+                    if (minLength == null || length < minLength)
+                    {
+                        minLength = length;
+                    }
+                }
+            }
+
+
+            if (minLength != null)
+            {
+                Console.WriteLine($"Минимальная длина из всех подмассивов с равномерно убывающей последовательностью равна {minLength}");
+            }
+            else
+            {
+                Console.WriteLine("Не найдено подмассивов с убывающей последовательностью");
+            }
+        }
+    }
+}
