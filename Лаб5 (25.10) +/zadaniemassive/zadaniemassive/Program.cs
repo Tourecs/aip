9ff8a0f6833d477ce69e3ef0e2fed04e874657f6
Tourecs <sverlov2006@mail.ru>
2025-01-03 03:44:33 +0600

    Add files via upload

---
+++ Лаб5 (25.10) +/zadaniemassive/zadaniemassive/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace aip
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите количество элементов массива:");
+                int n = Convert.ToInt32(Console.ReadLine());
+                string[] array_string = new string[n];
+                for (int i = 0; i < array_string.Length; i++)
+                {
+                    Console.WriteLine($"Введите элементы массива: {1 + i}");
+                    array_string[i] = Console.ReadLine();
+                }
+
+                int[] array = new int[array_string.Length];
+                for (int i = 0; i < array_string.Length; i++)
+                {
+                    array[i] = Convert.ToInt32(array_string[i]);
+                }
+
+                Console.WriteLine("Квадраты чисел массива:");
+                for (int i = 0; i < array_string.Length; i++)
+                {
+                    int square = array[i] * array[i];
+                    Console.WriteLine(square);
+                }
+
+            }
+        }
+    }
+}
