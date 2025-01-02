60000c53df22b648cf3e8a3307f94f1022732012
Tourecs <sverlov2006@mail.ru>
2025-01-03 03:44:09 +0600

    Add files via upload

---
+++ Лаб4 (11.10) +/zadanielab4/zadanielab4/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+namespace aip
+{
+    class Program
+    {
+        static void Main()
+        {
+            int result = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите значение:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                while (a > 0)
+                {
+                    if (a % 10 % 2 == 0)
+                    {
+                        a /= 10;
+                    }
+                    else
+                    {
+                        result = result * 10 + a % 10;
+                        a /= 10;
+                    }
+                }
+                if (result == 0)
+                {
+                    Console.WriteLine("Введено четное значение");
+                }
+                else
+                {
+                    Console.WriteLine("Введено нечетное значение:" + result);
+
+                    result = 0;
+                }
+
+
+            }
+        }
+    }
+}
