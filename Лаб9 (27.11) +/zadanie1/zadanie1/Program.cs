be0b2e8856b5bd0b4480991f7a90aadf178d3b37
Tourecs <sverlov2006@mail.ru>
2025-01-08 15:50:33 +0600

    Add files via upload

---
+++ Лаб9 (27.11) +/zadanie1/zadanie1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace aip
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку:");
+            string input = Console.ReadLine();
+            string k = "";
+            Regex regex = new Regex(@"a.b");
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                k += match.Value[1];
+            }
+
+            char mostfreqchar = FindMostFrequentChar(k);
+            Console.WriteLine($"Самый часто повторяющийся символ: {mostfreqchar}");
+        }
+
+        static char FindMostFrequentChar(string String)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char c in String)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+
+            char mostfreqchar = String[0];
+            int maxCount = 0;
+
+            foreach (var kvp in charCount)
+            {
+                if (kvp.Value > maxCount)
+                {
+                    maxCount = kvp.Value;
+                    mostfreqchar = kvp.Key;
+                }
+            }
+            return mostfreqchar;
+        }
+    }
+}
---
+++ Лаб9 (27.11) +/zadanie2/zadanie2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace aip
+{
+    class Program
+    {
+        static void Main()
+        {
+            string example = "aipaipaipaipaipaipaip";
+            int minlength = 3;
+            int maxlength = 0;
+
+            Console.WriteLine("Введите строку: ");
+            string input = Console.ReadLine();
+
+            int length = minlength;
+
+            while (length <= input.Length)
+            {
+                string temp = input.Substring(0, length);
+                if (example.Contains(temp))
+                {
+                    maxlength = length;
+                    length++;
+                }
+                else
+                {
+                    input = input.Substring(length - 1);
+                    length = minlength;
+                }
+            }
+
+            Console.WriteLine("Максимальная равна " + maxlength);
+        }
+    }
+}
